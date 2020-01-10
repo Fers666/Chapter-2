8dfaf68dc7a2a126617670ade6076e7e4be98abd
Fers666 <59649804+Fers666@users.noreply.github.com>
2020-01-10 14:59:51 +0300

    whilу,for,строки

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,18 @@
-a=5
-while a<=55 :
-    print(a)
-    a+=2
+a,b=int(input()),int(input())
+i=0
+if a==b :
+    s=a
+elif a<b :
+    i=a
+else :
+    i=b
+
+if a!=b :
+    while i<a*b :
+        if i%a==0 and i%b==0 :
+            s=i
+            break
+        else    :
+            i+=1
+            s=a*b
+print(s)
---
+++ lesson3.py
@@ -0,0 +1,17 @@
+a,b=input().split()
+a=int(a)
+b=int(b)
+s=0
+if a%2==0 :
+    a+=1
+for i in range (a,b+1,2) :
+    s+=i
+print(s)
+''' Выведите сумму всех нечетных числе от а до б'''
+a,b=(int(i) for i input().split())
+s=0
+if a%2==0 :
+    a+=1
+    for i int range (a,b+1,2)
+    s+=1
+print(s)
